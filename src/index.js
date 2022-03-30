fca7d9575b1a1722bdbd45618819637d9efd2ccb
Marco Budiongan <marcobdngn@gmail.com>
2022-03-30 23:40:13 +0800

    chore: setup basic methods

---
+++ src/index.js
@@ -0,0 +1,15 @@
+class CartManager {
+  constructor() {}
+
+  push() {}
+
+  update() {}
+
+  remove() {}
+
+  getCart() {
+    // Browser / in memory storage
+  }
+}
+
+module.exports = CartManager;
