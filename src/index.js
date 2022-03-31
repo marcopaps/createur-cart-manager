283a8f19a3c33290c73c597cdad59e3f1fe45c12
Marco Budiongan <marcobdngn@gmail.com>
2022-03-31 21:02:05 +0800

    feat: setup getCart method

--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
 import PouchDB from "pouchdb";
 class CartManager {
-  constructor() {}
+  constructor(dbName) {
+    this._db = new PouchDB(dbName);
+    this.cartItems = [];
+    this.totalPrice = 0;
+    this.totalQuantity = 0;
+    this.gst = 0;
+    this.getCart();
+  }
+
+  getCart() {
+    let _totalPrice = 0;
+    let _totalQuantity = 0;
+
+    return this._db
+      .allDocs({ include_docs: true })
+      .then((docs) => {
+        let subTotal = 0;
+
+        this.cartItems = docs.rows.map((item) => {
+          const itemData = item.doc;
+
+          subTotal = this._formatPrice(itemData.price * itemData.quantity);
+          _totalPrice += subTotal;
+          _totalQuantity += itemData.quantity;
+
+          return {
+            ...itemData,
+            subTotal,
+          };
+        });
+      })
+      .then(() => {
+        this.totalPrice = this._formatPrice(_totalPrice);
+        this.totalQuantity = _totalQuantity;
+
+        // ref: gst.calculatorsaustralia.com.au
+        this.gst = this._formatPrice(_totalPrice - _totalPrice / 1.1);
+
+        return {
+          cartItems: this.cartItems,
+          totalPrice: this.totalPrice,
+          totalQuantity: this.totalQuantity,
+          gst: this.gst,
+        };
+      });
+  }
 
   push() {}
 
   update() {}
 
   remove() {}
-
-  getCart() {
-    // Browser / in memory storage
-  }
 }
 
 module.exports = CartManager;
